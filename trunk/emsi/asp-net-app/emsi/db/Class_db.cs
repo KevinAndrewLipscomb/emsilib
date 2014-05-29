de5e1a609e81a158339d60c4c65eebe80fa54e8e
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2014-05-29 20:39:06 +0000

    Added ExecuteOneOffProcedureScriptWithTolerance().

--- trunk/emsi/asp-net-app/emsi/db/Class_db.cs
+++ trunk/emsi/asp-net-app/emsi/db/Class_db.cs
@@ -4,22 +4,47 @@ using System.Data;
 
 namespace Class_db
   {
+
   public class TClass_db
-      // The class should be abstract once all db operations in WebEMSOF are performed via descendants of this class.
+    // The class should be abstract once all db operations in WebEMSOF are performed via descendants of this class.
     {
+
     public MySqlConnection connection = null;
       // The connection member should be protected rather than public once all db operations in WebEMSOF are performed via descendants of this class.
 
     public TClass_db() : base()
       {
-      connection = new MySqlConnection();
-      connection.ConnectionString = ConfigurationManager.AppSettings["db_connection_string"];
+      connection = new MySqlConnection(connectionString:ConfigurationManager.AppSettings["db_connection_string"]);
       }
 
     public void Close()
       // This routine should be protected rather than public once all db operations in WebEMSOF are performed via descendants of this class.
       {
       connection.Close();
+      }
+
+    protected static void ExecuteOneOffProcedureScriptWithTolerance
+      (
+      string procedure_name,
+      MySqlScript my_sql_script
+      )
+      {
+      var done = false;
+      while (!done)
+        {
+        try
+          {
+          my_sql_script.Execute();
+          done = true;
+          }
+        catch (MySqlException the_exception)
+          {
+          if (the_exception.Message != "PROCEDURE " + procedure_name + " already exists")
+            {
+            throw;
+            }
+          }
+        }
       }
 
     public void Open()
