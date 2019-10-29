8e90baff4995524eea0c6209125a578f856bdbff
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2019-10-29 16:13:51 +0000

    Revised SetEmailAddress not to fail when a non-member user's email address needs to be associated with a member.

--- trunk/emsi/asp-net-app/emsi/db/Class_db_users.cs
+++ trunk/emsi/asp-net-app/emsi/db/Class_db_users.cs
@@ -312,11 +312,25 @@ namespace Class_db_users
         }
 
         public void SetEmailAddress(string id, string email_address)
-        {
-            this.Open();
-            new MySqlCommand(db_trail.Saved("UPDATE user " + "SET password_reset_email_address = \"" + email_address + "\"" + "WHERE id = " + id), this.connection).ExecuteNonQuery();
-            this.Close();
-        }
+          {
+          Open();
+          new MySqlCommand
+            (
+            db_trail.Saved
+              (
+              "START TRANSACTION"
+              + ";"
+              + " delete from user where (password_reset_email_address = '" + email_address + "') and (id not in (select user_id from user_member_map)) limit 1" // The limit should not be necessary but adds protection if another part of the query turns out to have been programmed erroneously.
+              + ";"
+              + " update user set password_reset_email_address = '" + email_address + "'" + " WHERE id = '" + id + "'"
+              + ";"
+              + " COMMIT"
+              ),
+            connection
+            )
+            .ExecuteNonQuery();
+          Close();
+          }
 
         public void SetPassword(string id, string encoded_password)
         {
